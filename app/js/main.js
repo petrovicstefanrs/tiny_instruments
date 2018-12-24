a3c052b653ea6f2815aac0f35316e60f8d2ee944
Stefan Petrovic <petrovic.stefan.rs@gmail.com>
2018-12-24 22:43:51 +0100

    init

---
+++ app/js/main.js
@@ -0,0 +1,116 @@
+/**
+ * CONSTANTS
+ * Set CONSTANTS
+ */
+
+const INSTRUMENTS_LIB = {
+	instruments: [
+		'violin',
+		'trumpet',
+		'keyboard',
+		'piano',
+		'sax',
+		'acoustic-guitar',
+		'electric-guitar',
+	],
+};
+
+const CYCLE_DIRECTIONS = {
+	left: 'left',
+	right: 'right',
+};
+
+const BASE_IMAGE = './images/';
+const BASE_SOUND = './music/';
+
+/**
+ * INSTRUMENT PLAYER HANDLER
+ * Handles playing and stopping intsruments
+ */
+
+let instrumentNumber = 0;
+let instrumentActive = false;
+let instrumentAudio = document.getElementById('instrumentAudio');
+let instrumentIcon = document.getElementById('instrumentIcon');
+
+const pulseAnimation = 'ti-AnimationPulse';
+const bodyAnimation = 'ti-AnimationBody';
+
+const setContent = () => {
+	instrumentIcon.style.backgroundImage = `url(${BASE_IMAGE}${
+		INSTRUMENTS_LIB.instruments[instrumentNumber]
+	}.png)`;
+
+	instrumentAudio.src = `${BASE_SOUND}${
+		INSTRUMENTS_LIB.instruments[instrumentNumber]
+	}.mp3`;
+};
+
+const stopInstrument = () => {
+	instrumentActive = false;
+	instrumentIcon.classList.remove(pulseAnimation);
+	document.body.classList.remove(
+		`${bodyAnimation}-${INSTRUMENTS_LIB.instruments[instrumentNumber]}`
+	);
+	instrumentAudio.pause();
+	instrumentAudio.currentTime = 0;
+};
+
+const startInstrument = () => {
+	if (!instrumentActive) {
+		instrumentIcon.classList.add(pulseAnimation);
+		document.body.classList.add(
+			`${bodyAnimation}-${INSTRUMENTS_LIB.instruments[instrumentNumber]}`
+		);
+		instrumentAudio.loop = true;
+		instrumentAudio.play();
+		instrumentActive = !instrumentActive;
+	} else {
+		stopInstrument();
+	}
+};
+
+instrumentIcon.addEventListener('click', startInstrument);
+
+/**
+ * CYCLE HANDLER
+ * Handles cycling through intsruments
+ */
+
+const BUTTON_LEFT = document.getElementById('arrow-left');
+const BUTTON_RIGHT = document.getElementById('arrow-right');
+
+const cycleInstruments = (direction = CYCLE_DIRECTIONS.left) => {
+	stopInstrument();
+
+	if (direction === CYCLE_DIRECTIONS.left) {
+		if (instrumentNumber <= 0) {
+			instrumentNumber = INSTRUMENTS_LIB.instruments.length - 1;
+		} else {
+			instrumentNumber--;
+		}
+	} else {
+		if (instrumentNumber >= INSTRUMENTS_LIB.instruments.length - 1) {
+			instrumentNumber = 0;
+		} else {
+			instrumentNumber++;
+		}
+	}
+
+	setContent();
+};
+
+BUTTON_LEFT.addEventListener('click', () =>
+	cycleInstruments(CYCLE_DIRECTIONS.left)
+);
+
+BUTTON_RIGHT.addEventListener('click', () =>
+	cycleInstruments(CYCLE_DIRECTIONS.right)
+);
+
+/**
+ * INIT
+ * Initialize the app
+ */
+
+document.addEventListener('onload', setContent());
---
+++ gulpfile.js
@@ -0,0 +1,145 @@
+// Utils
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const gutil = require('gulp-util');
+const useref = require('gulp-useref');
+const cache = require('gulp-cache');
+const del = require('del');
+const runSequence = require('run-sequence');
+
+// Css
+
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+
+// Babel + Browserify
+
+const babel = require('gulp-babel');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+
+// Other
+
+const imagemin = require('gulp-imagemin');
+
+// Development Tasks
+// -----------------
+
+// Start browserSync server
+
+gulp.task('browserSync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app',
+		},
+	});
+});
+
+gulp.task('sass', () => {
+	const plugins = [
+		autoprefixer({ browsers: ['last 2 versions'] }),
+		cssnano(),
+	];
+	return gulp
+		.src('app/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(plugins))
+		.pipe(gulp.dest('app/styles'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('babel', () => {
+	browserify({
+		entries: 'app/js/main.js',
+		debug: true,
+	})
+		.transform(babelify, { presets: ['@babel/preset-env'] })
+		.on('error', gutil.log)
+		.bundle()
+		.on('error', gutil.log)
+		.pipe(source('main.js'))
+		.pipe(gulp.dest('app/scripts'))
+		.pipe(browserSync.reload({ stream: true }));
+});
+
+// Watchers
+gulp.task('watch', () => {
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('app/js/**/*.js', ['babel']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/scripts/**/*.js', browserSync.reload);
+});
+
+// Optimization Tasks
+// ------------------
+
+// Optimizing CSS and JavaScript
+gulp.task('useref', function() {
+	return gulp
+		.src('app/*.html')
+		.pipe(useref())
+		.pipe(gulp.dest('dist'));
+});
+
+// Optimizing Images
+gulp.task('images', function() {
+	return (
+		gulp
+			.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+			// Caching images that ran through imagemin
+			.pipe(
+				cache(
+					imagemin({
+						interlaced: true,
+					})
+				)
+			)
+			.pipe(gulp.dest('dist/images'))
+	);
+});
+
+// Copying fonts
+gulp.task('fonts', function() {
+	return gulp.src('app/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+});
+
+// Copying sounds
+gulp.task('sounds', function() {
+	return gulp.src('app/music/**/*').pipe(gulp.dest('dist/music'));
+});
+
+// Cleaning
+gulp.task('clean', function() {
+	return del.sync('dist').then(function(cb) {
+		return cache.clearAll(cb);
+	});
+});
+
+gulp.task('clean:dist', function() {
+	return del.sync(
+		['dist/**/*', '!dist/images', '!dist/images/**/*'],
+		'!dist/music',
+		'!dist/music/**/*'
+	);
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+	runSequence(['sass', 'babel', 'browserSync'], 'watch', callback);
+});
+
+gulp.task('build', function(callback) {
+	runSequence(
+		'clean:dist',
+		'babel',
+		'sass',
+		['useref', 'images', 'fonts', 'sounds'],
+		callback
+	);
+});
